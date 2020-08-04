46ef69b7a13de8354c50f3921dc2516b86f624b8
unknown <fabioalexandercastro@gmail.com>
2020-08-04 15:16:59 -0500

    create categories

---
+++ 04_creatingCategories.R
@@ -0,0 +1,23 @@
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(raster, rgdal, rgeos, stringr, terra, sf, tidyverse, ggspatial)
+
+rm(list = ls())
+options(scipen = 999)
+
+# Load data ---------------------------------------------------------------
+list.files('../t')
+
+
+
+
+
+
+
+
+
+
+
+
+
