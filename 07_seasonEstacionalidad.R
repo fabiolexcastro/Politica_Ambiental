bafa4ca39f83f74bc90a88b93f343e197a65702a
unknown <fabioalexandercastro@gmail.com>
2020-08-10 10:46:47 -0500

    Estacionalidad de los puntos de calor

--- 07_seasonEstacionalidad.R
+++ 07_seasonEstacionalidad.R
@@ -22,17 +22,16 @@ fire <- st_read('../shp/fire/mpio/fire_modis_veredas.shp')
 pnts <- st_read('../shp/fire/mpio/fire_points.shp')%>% 
   mutate(CONFIDENCE = as.numeric(CONFIDENCE))
 
-# Confiability graph ------------------------------------------------------
-
+# Tidy the table
 tble <- pnts %>% 
   as.data.frame %>% 
   dplyr::select(-geometry) %>% 
   as_tibble() %>% 
-  dplyr::select(LONGITUDE, LATITUDE, ACQ_DATE, ACQ_TIME, CONFIDENCE) %>% 
+  dplyr::select(LONGITUDE, LATITUDE, BRIGHTNESS, ACQ_DATE, ACQ_TIME, CONFIDENCE) %>% 
   mutate(CONFIDENCE = as.numeric(CONFIDENCE)) %>% 
-  mutate(CONFIDENCE = ifelse(is.na(CONFIDENCE), mean(tble$CONFIDENCE, na.rm = TRUE), CONFIDENCE))
+  mutate(CONFIDENCE = ifelse(is.na(CONFIDENCE), mean(CONFIDENCE, na.rm = TRUE), CONFIDENCE))
 
-# To remove Outliers
+# To remove the outliers --------------------------------------------------
 nrow(tble)
 nrow(drop_na(tble))
 norm <- scores(tble[,c('CONFIDENCE')]) 
@@ -54,5 +53,37 @@ gghist <- ggplot(data = tble, aes(x = CONFIDENCE)) +
 ggsave(plot = gghist, filename = '../png/graphs/Histograma confiabilidad.png', units = 'in',
        width = 12, height = 9, dpi = 300)
 
-# To make the season graph ------------------------------------------------
+write.csv(tble, '../tbl/hotspots/hotspots_removeOTL.csv', row.names = FALSE)
 
+# Time line graph ------------------------------------------------------
+tble <- tble %>% 
+  mutate(month = month(ACQ_DATE),
+         year = year(ACQ_DATE),
+         day = day(ACQ_DATE))
+smmr <- tble %>% 
+  group_by(month) %>% 
+  dplyr::summarise(count = n()) %>% 
+  ungroup() %>% 
+  inner_join(., data.frame(month = 1:12, month_abb = month.abb), by = 'month') %>% 
+  dplyr::select(month_abb, count) %>% 
+  mutate(month_abb = factor(month_abb, levels = month.abb))
+lbl <- data.frame(month_abb = month.abb,
+                  month = c('Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'))
+smmr <- inner_join(smmr, lbl, by = 'month_abb')
+smmr <- smmr %>% dplyr::select(month, count) %>% mutate(month = factor(month, levels = c('Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic')))
+
+gg_count <- ggplot(data = smmr, aes(x = month, y = count)) +
+  geom_col() +
+  labs(x = '',
+       y = 'Conteo puntos de calor',
+       caption ='Adaptado de NASA - FIRMS') +
+  ggtitle(label = 'Cantidad de puntos de calor - mes (2001 a 2019)\nMonterrey - Casanare') +
+  theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5))
+ggsave(plot = gg_count, 
+       filename = '../png/graphs/Puntos de calor por mes.png', 
+       units = 'in', width = 12, height = 9, dpi = 300)
+
+write.csv(smmr, '../tbl/hotspots/count_months_summarise.csv', row.names = FALSE)
+
+summary(tble$BRIGHTNESS)
+sd(tble$BRIGHTNESS)
