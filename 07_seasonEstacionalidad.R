96f15101e0aa845a8f3b74a2565760d57148795b
unknown <fabioalexandercastro@gmail.com>
2020-08-10 10:30:10 -0500

    Grafica de confiabilidad para hotspots

--- 07_seasonEstacionalidad.R
+++ 07_seasonEstacionalidad.R
@@ -1,9 +1,8 @@
-
-
+ 
 # Load libraries ----------------------------------------------------------
 require(pacman)
 pacman::p_load(raster, rgdal, rgeos, stringr, sf, tidyverse, lubridate,
-               RColorBrewer, ggspatial)
+               RColorBrewer, ggspatial, outliers)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -23,13 +22,37 @@ fire <- st_read('../shp/fire/mpio/fire_modis_veredas.shp')
 pnts <- st_read('../shp/fire/mpio/fire_points.shp')%>% 
   mutate(CONFIDENCE = as.numeric(CONFIDENCE))
 
-# To make the season graph ------------------------------------------------
-tble <- fire %>% 
+# Confiability graph ------------------------------------------------------
+
+tble <- pnts %>% 
   as.data.frame %>% 
   dplyr::select(-geometry) %>% 
-  dplyr::select(LONGITUDE, LATITUDE, ACQ_DATE, ACQ_TIME, CONFIDENCE) %>% 
   as_tibble() %>% 
-  mutate(CONFIDENCE = parse_number(CONFIDENCE))
-  
-hist(tble$CONFIDENCE)
+  dplyr::select(LONGITUDE, LATITUDE, ACQ_DATE, ACQ_TIME, CONFIDENCE) %>% 
+  mutate(CONFIDENCE = as.numeric(CONFIDENCE)) %>% 
+  mutate(CONFIDENCE = ifelse(is.na(CONFIDENCE), mean(tble$CONFIDENCE, na.rm = TRUE), CONFIDENCE))
+
+# To remove Outliers
+nrow(tble)
+nrow(drop_na(tble))
+norm <- scores(tble[,c('CONFIDENCE')]) 
+norm_na <- norm
+norm_na[abs(norm_na) > 3.5] <- NA
+names(norm_na) <- 'normal'
+normpoints <- cbind(tble, normal = norm_na) %>% 
+  as_data_frame()
+tble <- normpoints
+tble <- tble[!is.na(tble$normal),]
+
+gghist <- ggplot(data = tble, aes(x = CONFIDENCE)) +
+  geom_histogram(bins = 20) +
+  labs(x = 'Confiabilidad (%)', 
+       y = 'Frecuencia',
+       caption = 'Adaptado de NASA - FIRMS') +
+  ggtitle(label = 'Histograma de confiabilidad para los puntos de calor') +
+  theme(plot.title = element_text(size = 16, hjust = 0.5, face = 'bold'))
+ggsave(plot = gghist, filename = '../png/graphs/Histograma confiabilidad.png', units = 'in',
+       width = 12, height = 9, dpi = 300)
+
+# To make the season graph ------------------------------------------------
 
