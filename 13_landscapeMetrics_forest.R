9ea2fe793d9f5ca278fc3ab73e2704b5e50fd365
unknown <fabioalexandercastro@gmail.com>
2020-08-24 14:59:25 -0500

    update landscape metrics

--- 13_landscapeMetrics_forest.R
+++ 13_landscapeMetrics_forest.R
@@ -6,7 +6,7 @@ pacman::p_load(raster, rgdal, rgeos, stringr, sf, tidyverse, landscapemetrics)
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999)
-landscapemetrics
+
 # Load data ---------------------------------------------------------------
 frs <- raster('../tif/forest//hansen/Hansen_GFC-2019-v1.7_treecover2000_10N_080W.tif')
 lss <- raster('../tif/forest/hansen/Hansen_GFC-2019-v1.7_lossyear_10N_080W.tif')
@@ -43,26 +43,79 @@ plot(frs, main = 2000)
 plot(rsl, main = 2010)
 par(mfrow = c(1,1))
 
-writeRaster(rsl, '../tif/forest/hansen/forest_noforest_2010.tif')
-
+writeRaster(rsl, '../tif/forest/hansen/forest_noforest_2010.tif', overwrite = TRUE)
 
 # Getting the landscape metrics -------------------------------------------
 prj <- '+proj=tmerc +lat_0=4.596200416666666 +lon_0=-74.07750791666666 +k=1 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 crs(rsl) <- crs(lim)
 rsl <- projectRaster(rsl, crs = prj)
 rsl <- rsl * 0 + 1
+frs <- rsl
 
 # Cover layer
 cov <- shapefile('../shp/cobertura/cobertura_monterrey.shp')
 
 # Rasterize cover layer
+cov_tbl <- data.frame(
+  gid = 1:length(unique(cov$LEYENDA3N)),
+  type_1 = str_sub(unique(cov$LEYENDA3N), start = 7, end = nchar(unique(cov$LEYENDA3N))),
+  type_2 = unique(cov$LEYENDA3N)
+                   )
+cov <- inner_join(st_as_sf(cov), cov_tbl, by = c('LEYENDA3N' = 'type_2'))
+cov <- as(cov, 'Spatial')
+crs(cov) <- crs(lim)
+cov <- spTransform(x = cov, CRSobj = prj)
 
+compareCRS(x = cov, y = rsl)
 
+cov_lyr <- rasterize(x = cov, y = frs, field = 'gid')
+write.csv(cov_tbl, '../tbl/cobertura/leyenda_cobertura.csv', row.names = FALSE)
+writeRaster(cov_lyr, '../tif/cover/coverlayer_2010.tif')
+
+rsl <- cov_lyr * frs
+writeRaster(rsl, '../tif/cover/coverlayer_forest_2010.tif')
+res <- res(rsl)[1] * res(rsl)[1]
+
+rsl_has <- data.frame(table(rsl[])) %>% 
+  mutate(Var1 = as.numeric(Var1)) %>% 
+  inner_join(., cov_tbl[,1:2], by = c('Var1' = 'gid')) %>% 
+  setNames(c('gid', 'Frequency', 'Cobertura')) %>% 
+  mutate(meters = Frequency * res,
+         hectares = round(meters / 10000, 0)) %>% 
+  mutate(porc = hectares / sum(hectares) * 100,
+         porc = round(porc, 1)) %>% 
+  dplyr::select(gid, Cobertura, hectares, porc) %>% 
+  arrange(desc(porc))
+rsl_has <- rsl_has %>% filter(porc > 0.3)
+
+write.csv(rsl_has, '../tbl/cobertura/leyenda_cobertura_filter.csv', row.names = FALSE)
+
+rsl_has <- read_csv('../tbl/cobertura/leyenda_cobertura_filter.csv')
+rsl_ha2 <- rsl_has %>% 
+  group_by(Cobertura_RCL) %>% 
+  summarize(hectares = sum(hectares)) %>% 
+  ungroup() %>% 
+  mutate(porc = hectares / sum(hectares) * 100) %>% 
+  arrange(desc(porc))
 
 
 # Perimeter
 perim <- lsm_p_perim(rsl)
-areas <- lsp_p_area(rsl)
+areas <- lsm_p_area(rsl)
+
+# Patches and boundaries
+bld_blck <- get_patches(rsl)
+bld_bnds <- get_boundaries(rsl)[[1]]
+
+cat_01 <- bld_blck$`1`
+cat_01 <- rasterToPolygons(cat_01)
+shapefile(cat_01, '../shp/cover_forest/2010/category_01.shp')
+
+# Enn landscape metrics
+lsm_enn <- rsl %>% lsm_p_enn(., directions = 8)
+
+lsm_enn %>% filter(class == 1)
+
 
 
 
