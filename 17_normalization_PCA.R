f1d71265b2aabd4b06f841b48be8a7b8d700e6ad
unknown <fabioalexandercastro@gmail.com>
2020-10-15 09:42:41 -0500

    normalization pca and some graphs

--- 17_normalization_PCA.R
+++ 17_normalization_PCA.R
@@ -1,7 +1,7 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(raster, missMDA, factoextra, FactoMineR, rgdal, rgeos, stringr, sf, tidyverse, fasterize, landscapemetrics, outliers)
+pacman::p_load(raster, devtools, missMDA, factoextra, FactoMineR, rgdal, rgeos, stringr, sf, tidyverse, fasterize, landscapemetrics, outliers)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -9,6 +9,7 @@ options(scipen = 999)
 
 # Load data ---------------------------------------------------------------
 tbl <- list.files('../tbl/metrics', full.names = TRUE, pattern = '.csv$')
+tbl <- tbl[-grep('metrics_all_normalization', tbl, value = FALSE)]
 nms <- basename(tbl) %>% gsub('.csv', '', .)
 lbl <- read_csv('../tbl/labels_metrics.csv')
 
@@ -73,25 +74,47 @@ res.pca.2000 <- PCA(tbl.2000[,4:20])
 plot(res.pca.2000)
 plot.PCA(res.pca.2000, choix = 'in', invisible = 'ind.sup')
 
-
 png(filename = '../png/graphs/pca/pca_analysis.png', units = 'in', width = 12, height = 9, res = 300)
 plot.PCA(res.pca.2000, choix = 'var', invisible = 'quanti.sup')
 dev.off()
-round(cbind(res.pca.2000$var$coord[,1:3], res.pca.2000$var$cos2[,1:3], res.pca.2000$var$contrib[,1:3]), 2)
 
 res.hcpc <- HCPC(res.pca.2000)
 
-fviz_eig(res.pca.2000, addlabels = TRUE, hjust = -0.3) + 
-  theme_bw()
+# Contribucion de las dimensiones a la explicacion de la varianza total de las variables
+gg.scr.00 <- fviz_eig(res.pca.2000, addlabels = TRUE, hjust = -0.3) + 
+  theme_bw() +
+  labs(x = 'Componentes', 
+       y = 'Contribución de explicación de\n las varianzas') +
+  ggtitle(label = '') +
+  theme(axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11),
+        axis.text.y = element_text(size = 11),
+        axis.title.y = element_text(size = 12, face = 'bold'),
+        axis.title.x = element_text(size = 12, face = 'bold'))
+ggsave(plot = gg.scr.00, filename = '../png/graphs/pca/Scree Plot 2000.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
 
-fviz_pca(res.pca.2000, label = 'var')
+# Identificar cual es la contribucion de cada variable (metrica) a la varianza / variabilidad de todo mi pull de datos
+contrib.00 <- round(cbind(res.pca.2000$var$contrib[,1:2]), 2) %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  setNames(c('metrica', paste0('dim_', 1:2))) %>% 
+  as_tibble() %>% 
+  arrange(desc(dim_1))
 
-gg2 <- fviz_pca_biplot(res.pca.2000, 
-                       label= "var", 
-                       habillage = res.hcpc$data.clust$clust, 
-                       addEllipses = TRUE, 
-                       ellipse.level = 0.95) + 
-  theme_bw()
+gg.cnt.00 <- fviz_pca_var(res.pca.2000,
+                          col.var = "contrib", # Color by contributions to the PC
+                          gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                          repel = TRUE) + # Avoid text overlapping
+  labs(color = 'Contribución',
+       x = 'Componente 1 (63.6%)',
+       y = 'Componente 2 (21.6%)') +
+  theme(legend.position = 'bottom',
+        legend.key.width = unit(3, 'line'))
+ggsave(plot = gg.cnt.00, filename = '../png/graphs/pca/pca_contrib_2000.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
+
+write.csv(contrib.00, '../tbl/pca/contrib_metricas_2000.csv', row.names = FALSE)
+
 # 2005 -------------------------------------------
 
 
