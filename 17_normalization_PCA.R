f99c31c559f84dff7daa1727b15b7a3430158a57
unknown <fabioalexandercastro@gmail.com>
2020-10-16 09:38:14 -0500

    PCA Analysis

--- 17_normalization_PCA.R
+++ 17_normalization_PCA.R
@@ -1,7 +1,7 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(raster, devtools, missMDA, factoextra, FactoMineR, rgdal, rgeos, stringr, sf, tidyverse, fasterize, landscapemetrics, outliers)
+pacman::p_load(raster, missMDA, ggpubr, factoextra, FactoMineR, rgdal, rgeos, stringr, sf, tidyverse, fasterize, landscapemetrics, outliers)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -85,11 +85,12 @@ gg.scr.00 <- fviz_eig(res.pca.2000, addlabels = TRUE, hjust = -0.3) +
   theme_bw() +
   labs(x = 'Componentes', 
        y = 'Contribución de explicación de\n las varianzas') +
-  ggtitle(label = '') +
+  ggtitle(label = 'Contribución de las componentes - Año 2000') +
   theme(axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11),
         axis.text.y = element_text(size = 11),
         axis.title.y = element_text(size = 12, face = 'bold'),
-        axis.title.x = element_text(size = 12, face = 'bold'))
+        axis.title.x = element_text(size = 12, face = 'bold'), 
+        plot.title = element_text(size =12, face = 'bold', hjust = 0.5))
 ggsave(plot = gg.scr.00, filename = '../png/graphs/pca/Scree Plot 2000.png',
        units = 'in', width = 9, height = 6, dpi = 300)
 
@@ -108,30 +109,119 @@ gg.cnt.00 <- fviz_pca_var(res.pca.2000,
   labs(color = 'Contribución',
        x = 'Componente 1 (63.6%)',
        y = 'Componente 2 (21.6%)') +
-  theme(legend.position = 'bottom',
-        legend.key.width = unit(3, 'line'))
+  theme(legend.position = 'none',
+        legend.key.width = unit(3, 'line')) +
+  ggtitle(label = '')
 ggsave(plot = gg.cnt.00, filename = '../png/graphs/pca/pca_contrib_2000.png',
        units = 'in', width = 9, height = 6, dpi = 300)
 
 write.csv(contrib.00, '../tbl/pca/contrib_metricas_2000.csv', row.names = FALSE)
 
 # 2005 -------------------------------------------
+tbl.2005 <- tbl %>% filter(year == 2005)
+res.pca.2005 <- PCA(tbl.2005[,4:20])
 
+# Contribucion de las dimensiones a la explicacion de la varianza total de las variables
+gg.scr.05 <- fviz_eig(res.pca.2005, addlabels = TRUE, hjust = -0.3) + 
+  theme_bw() +
+  labs(x = 'Componentes', 
+       y = 'Contribución de explicación de\n las varianzas') +
+  ggtitle(label = 'Contribución de las componentes - Año 2005') +
+  theme(axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11),
+        axis.text.y = element_text(size = 11),
+        axis.title.y = element_text(size = 12, face = 'bold'),
+        axis.title.x = element_text(size = 12, face = 'bold'), 
+        plot.title = element_text(size =12, face = 'bold', hjust = 0.5))
+ggsave(plot = gg.scr.05, filename = '../png/graphs/pca/Scree Plot 2005.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
+
+# Identificar cual es la contribucion de cada variable (metrica) a la varianza / variabilidad de todo mi pull de datos
+contrib.05 <- round(cbind(res.pca.2005$var$contrib[,1:2]), 2) %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  setNames(c('metrica', paste0('dim_', 1:2))) %>% 
+  as_tibble() %>% 
+  arrange(desc(dim_1))
+
+gg.cnt.05 <- fviz_pca_var(res.pca.2005,
+                          col.var = "contrib", # Color by contributions to the PC
+                          gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                          repel = TRUE) + # Avoid text overlapping
+  labs(color = 'Contribución',
+       x = 'Componente 1 (69.3%)',
+       y = 'Componente 2 (20.2%)') +
+  theme(legend.position = 'none',
+        legend.key.width = unit(3, 'line')) +
+  ggtitle(label = '')
+ggsave(plot = gg.cnt.05, filename = '../png/graphs/pca/pca_contrib_2005.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
+
+write.csv(contrib.05, '../tbl/pca/contrib_metricas_2005.csv', row.names = FALSE)
 
 # 2010 -------------------------------------------
+tbl.2010 <- tbl %>% filter(year == 2010)
+res.pca.2010 <- PCA(tbl.2010[,4:20])
+
+# Contribucion de las dimensiones a la explicacion de la varianza total de las variables
+gg.scr.10 <- fviz_eig(res.pca.2010, addlabels = TRUE, hjust = -0.3) + 
+  theme_bw() +
+  labs(x = 'Componentes', 
+       y = 'Contribución de explicación de\n las varianzas') +
+  ggtitle(label = 'Contribución de las componentes - Año 2010') +
+  theme(axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11),
+        axis.text.y = element_text(size = 11),
+        axis.title.y = element_text(size = 12, face = 'bold'),
+        axis.title.x = element_text(size = 12, face = 'bold'), 
+        plot.title = element_text(size =12, face = 'bold', hjust = 0.5))
+ggsave(plot = gg.scr.10, filename = '../png/graphs/pca/Scree Plot 2010.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
+
+contrib.10 <- round(cbind(res.pca.2010$var$contrib[,1:2]), 2) %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  setNames(c('metrica', paste0('dim_', 1:2))) %>% 
+  as_tibble() %>% 
+  arrange(desc(dim_1))
+
+gg.cnt.10 <- fviz_pca_var(res.pca.2010,
+                          col.var = "contrib", # Color by contributions to the PC
+                          gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                          repel = TRUE) + # Avoid text overlapping
+  labs(color = 'Contribución',
+       x = 'Componente 1 (69%)',
+       y = 'Componente 2 (20.4%)') +
+  theme(legend.position = 'bottom',
+        legend.key.width = unit(3, 'line')) +
+  ggtitle(label = '')
+ggsave(plot = gg.cnt.10, filename = '../png/graphs/pca/pca_contrib_2010.png',
+       units = 'in', width = 9, height = 6, dpi = 300)
+
+write.csv(contrib.10, '../tbl/pca/contrib_metricas_2005.csv', row.names = FALSE)
+
+# Join the pca graphics --------------------------------------------------
+gg.all <- ggarrange(gg.cnt.00, gg.cnt.05, gg.cnt.10, ncol = 1, nrow = 3, labels = c('PCA 2000', 'PCA 2005', 'PCA 2010'))
+ggsave(plot = gg.all, filename = '../png/graphs/pca/pca_contrib_years.png',
+       units = 'in', width = 5, height = 14, dpi = 300)
+
+# Tidy the table contributino --------------------------------------------
+contrib.00 <- contrib.00 %>% setNames(c('metrica', 'dim_1_2000', 'dim_2_2000'))
+contrib.05 <- contrib.05 %>% setNames(c('metrica', 'dim_1_2005', 'dim_2_2005'))
+contrib.10 <- contrib.10 %>% setNames(c('metrica', 'dim_1_2010', 'dim_2_2010'))
+
+contrib <- list(contrib.00, contrib.05, contrib.10) %>% 
+  purrr::reduce(inner_join, by = 'metrica') 
+  
+write.csv(contrib, '../tbl/pca/contrib_metricas_all.csv', row.names = FALSE)
+
+contrib <- contrib %>% 
+  gather(var, value, -metrica) %>% 
+  mutate(year = str_sub(var, 7, 10),
+         dimension = str_sub(var, 1, 5) %>% str_replace(., '_', ' ')) %>% 
+  dplyr::select(metrica, dimension, year, value)
+
+
+# Scree plot - all in only one -----------------------------------------------
+gg_contrib <- ggplot(data = aes(x = ))
 
 
 # End ---------------------------------------------------------------------
-res.pca <- PCA(tbl[,4:20])
-plot(res.pca)
-round(res.pca$eig, 2) # Las dos primeras componentes registran un porcentaje de varianza del 87.39% del total de los datos
-# Los dos primeros ejes (componentes) expresan 87.39% de la inercia total; en toros términos, este porcentaje de la variabilidad total de la nube
-# de los individuos (o de las variables es extremadamente importante y está representado por el pirmer plano, que simboliza la variable contenida en el conjunto del juego 
-# activo de datos. 
-
-plot.PCA(res.pca, choix = 'in', invisible = 'ind.sup')
-
-
-lapply(dimdesc(res.pca), lapply, round, 2)
-
-barplot(res.pca$eig[,1], main = 'Valores propios', names.arg = paste('dim', 1:nrow(res.pca$eig)))
