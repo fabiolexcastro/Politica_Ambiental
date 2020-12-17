c1c782b7cb310363e928e0f136f94ce6ae3a6231
unknown <fabioalexandercastro@gmail.com>
2020-12-17 15:02:28 -0500

    update

--- 35_mapsClimate_v2.R
+++ 35_mapsClimate_v2.R
@@ -15,45 +15,83 @@ crn <- list.files('../tif/climate/current/worldclim_v2/stack', full.names = TRUE
 crn <- mixedsort(crn)
 vrs <- c('prec', 'tmean', 'tmax', 'tmin')  
 lim <- shapefile('../shp/base/veredas_mas_monterrey.shp')
+vrd <- shapefile('../shp/base/veredas_ok.shp')
 
 crn <- stack(crn)
 
-# RCP 4.5 -----------------------------------------------------------------
+# RCP 4.5 2030s  ---------------------------------------------------------
 ftr <- list.files('../workspace/climate/raster/ensemble/rcp_4.5/2030s', full.names = TRUE)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
 ftr <- mixedsort(ftr)
 ftr <- stack(ftr)
 
-make_stack <- function(cr, ft){
-  
-  cr <- crn
-  ft <- ftr
-  
-  names(cr) <- paste0('crn_', names(cr))
-  names(ft) <- paste0('ftr_', names(ft))
-  
-  ms.cr <- cr[[1]] * 0
-  ms.ft <- ft[[1]] * 0
-  
-  print('Stacking current - future')
-  ft <- raster::crop(ft, ms.cr) %>% raster::mask(., ms.cr)
-  st <- addLayer(cr, ft)
-  
-  print('Raster to table')
-  tb <- rasterToPoints(st, spatial = FALSE) %>% as_tibble()
-  
-  print('Tidy to table')
-  tb <- tb %>% mutate(gid = 1:nrow(.)) %>% gather(var, value, -gid, -x, -y) 
-  tb <- tb %>% mutate(period = str_sub(var, 1, 3))
-  tb <- tb %>% mutate(variable = str_sub(var, 5, 8))
-  tb <- tb %>% mutate(month = parse_number(var))
-  tb <- tb %>% dplyr::select(gid, x, y, period, variable, month, value)
-  tb <- tb %>% spread(period, value)
-  tb <- tb %>% mutate(ftr = ifelse(variable %in% c('tmax', 'tmea', 'tmin'), ftr / 10, ftr))
-  tb <- tb %>% mutate(difference = ftr - crn)
-  tb <- tb %>% mutate(porcentaje = (difference / crn) * 100)
-  
-}
+make_stack(cr = crn, ft = ftr)
 
+# RCP 4.5 2050s -----------------------------------------------------------
+ftr <- list.files('../workspace/climate/raster/ensemble/rcp_4.5/2050s', full.names = TRUE)
+ftr <- mixedsort(ftr)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+rcp <- unique(rcp)
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
+ftr <- stack(ftr)
 
+make_stack(cr = crn, ft = ftr)
 
+# RCP 4.5 2080s -----------------------------------------------------------
+ftr <- list.files('../workspace/climate/raster/ensemble/rcp_4.5/2080s', full.names = TRUE)
+ftr <- mixedsort(ftr)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+rcp <- unique(rcp)
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
+ftr <- stack(ftr)
+
+make_stack(cr = crn, ft = ftr)
+
+# RCP 8.5 2030s  ---------------------------------------------------------
+ftr <- list.files('../workspace/climate/raster/ensemble/rcp_8.5/2030s', full.names = TRUE)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
+ftr <- mixedsort(ftr)
+ftr <- stack(ftr)
+
+make_stack(cr = crn, ft = ftr)
+
+# RCP 8.5 2050s -----------------------------------------------------------
+ftr <- list.files('../workspace/climate/raster/ensemble/rcp_8.5/2050s', full.names = TRUE)
+ftr <- mixedsort(ftr)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+rcp <- unique(rcp)
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
+ftr <- stack(ftr)
+
+make_stack(cr = crn, ft = ftr)
+
+# RCP 8.5 2080s -----------------------------------------------------------
+ftr <- list.files('../workspace/climate/raster/ensemble/rcp_8.5/2080s', full.names = TRUE)
+ftr <- mixedsort(ftr)
+rcp <- str_split(string = ftr, pattern = '/')
+rcp <- sapply(1:length(rcp), function(k) rcp[[k]][6])
+rcp <- unique(rcp)
+yea <- str_split(string = ftr, pattern = '/')
+yea <- sapply(1:length(yea), function(k) yea[[k]][7])
+yea <- unique(yea)
+ftr <- stack(ftr)
+
+make_stack(cr = crn, ft = ftr)
 
